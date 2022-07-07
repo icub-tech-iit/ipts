cfb548110c16c610ae8e3255095c9d119ac04c4d
davidetome <davide.tome@iit.it>
2022-07-07 15:01:05 +0200

    strain2c - added ipts test support

---
+++ ipts-test-suite/iCubProductionTestSuite/iCubProductionTestSuite/tools/boards/strain2/flash/strain2c_test.bat
@@ -0,0 +1,21 @@
+:start
+@echo off
+
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+
+if %OS%==32BIT set PATH=%ProgramFiles%
+if %OS%==64BIT set PATH=%ProgramFiles(x86)%
+
+"%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -ME
+echo;
+"%PATH%\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility\ST-LINK_CLI.exe" -P "hex\strain2_revC_test_v1B0.hex" -Rst -Run
+echo "PRESS ENTER TO CONTINUE"
+echo;
+IF %errorlevel% NEQ 0 GOTO :error
+GOTO :end
+:error
+echo There was an error.
+EXIT 1
+:end
+echo End.
+EXIT 0
