42275e80fbfb597a94935f8e0eead6f065e14c2c
davidetome <davide.tome@iit.it>
2023-06-22 13:16:04 +0200

    fixed ping test

--- ipts-test-suite/iCubProductionTestSuite/iCubProductionTestSuite/tools/boards/mc4plus/eth/test_eth.bat
+++ ipts-test-suite/iCubProductionTestSuite/iCubProductionTestSuite/tools/boards/mc4plus/eth/test_eth.bat
@@ -1,5 +1,11 @@
+:start
+@echo off
+
 Rem ping an IP address and check the result - davide.tome@iit.it
 @echo off
+
+if %OS%==32BIT set PATH=%ProgramFiles%
+if %OS%==64BIT set PATH=%ProgramFiles(x86)%
 
 set ip=10.0.1.99
 %SystemRoot%\system32\ping.exe -n 1 %ip% >nul
@@ -12,3 +18,4 @@ EXIT 0
 
 :error
 echo Test Failed! %ip% is NOT availabl
+EXIT 1
