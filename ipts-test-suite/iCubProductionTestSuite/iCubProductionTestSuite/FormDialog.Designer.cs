aac29092c245526c30c10a42f44b8e44bc2240f3
Davide Tomé <davide.tome@iit.it>
2024-01-26 15:21:44 +0100

    IPTS - v1.5.2 (#35)

--- ipts-test-suite/iCubProductionTestSuite/iCubProductionTestSuite/FormDialog.Designer.cs
+++ ipts-test-suite/iCubProductionTestSuite/iCubProductionTestSuite/FormDialog.Designer.cs
@@ -40,7 +40,7 @@
             // 
             this.buttonOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonOk.Location = new System.Drawing.Point(1109, 851);
+            this.buttonOk.Location = new System.Drawing.Point(1196, 853);
             this.buttonOk.Name = "buttonOk";
             this.buttonOk.Size = new System.Drawing.Size(135, 42);
             this.buttonOk.TabIndex = 2;
@@ -52,7 +52,7 @@
             // 
             this.buttonYes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonYes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonYes.Location = new System.Drawing.Point(1025, 851);
+            this.buttonYes.Location = new System.Drawing.Point(1469, 853);
             this.buttonYes.Name = "buttonYes";
             this.buttonYes.Size = new System.Drawing.Size(135, 42);
             this.buttonYes.TabIndex = 3;
@@ -64,7 +64,7 @@
             // 
             this.buttonNo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonNo.Location = new System.Drawing.Point(1197, 851);
+            this.buttonNo.Location = new System.Drawing.Point(1284, 853);
             this.buttonNo.Name = "buttonNo";
             this.buttonNo.Size = new System.Drawing.Size(135, 42);
             this.buttonNo.TabIndex = 4;
@@ -77,8 +77,8 @@
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox1.Location = new System.Drawing.Point(-122, 1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(967, 844);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.Size = new System.Drawing.Size(1175, 833);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -86,7 +86,7 @@
             // 
             this.textBox1.Enabled = false;
             this.textBox1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(851, 12);
+            this.textBox1.Location = new System.Drawing.Point(1068, 1);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(632, 833);
@@ -96,7 +96,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1495, 917);
+            this.ClientSize = new System.Drawing.Size(1707, 917);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.buttonNo);
             this.Controls.Add(this.buttonYes);
