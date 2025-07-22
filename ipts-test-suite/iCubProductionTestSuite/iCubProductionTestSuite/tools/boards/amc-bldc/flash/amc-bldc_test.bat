81435f3b526883e86ddafde6743c28922210f573
Jacopo Losi <jacopo.losi@iit.it>
2025-07-22 11:20:37 +0200

    Add check for error level when programming OB

--- ipts-test-suite/iCubProductionTestSuite/iCubProductionTestSuite/tools/boards/amc-bldc/flash/amc-bldc_test.bat
+++ ipts-test-suite/iCubProductionTestSuite/iCubProductionTestSuite/tools/boards/amc-bldc/flash/amc-bldc_test.bat
@@ -3,9 +3,10 @@
 
 set PATH=C:\Program Files
 
-echo "First Program User Configuration Option Bytes DBANK and nSWBOOT0";
+echo "Program User Configuration Option Bytes DBANK and nSWBOOT0";
 "%PATH%\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe" -c port=SWD -ob DBANK=0x0 nSWBOOT0=1 -Rst -Run
 echo;
+IF %errorlevel% NEQ 0 GOTO :error
 "%PATH%\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe" -c port=SWD freq=8000 ap=0 reset=SWrst
 echo;
 "%PATH%\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe" -c port=SWD -d "hex\amcbldc.test.hex" 0x08000000 --verify
