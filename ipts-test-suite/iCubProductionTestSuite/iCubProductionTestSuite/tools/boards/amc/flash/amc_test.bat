f6845d0dbf18fc62754a9ec6717bb8ac217b509c
Jacopo Losi <jacopo.losi@iit.it>
2025-07-25 11:29:55 +0200

    Finalize partial tests amc

---
+++ ipts-test-suite/iCubProductionTestSuite/iCubProductionTestSuite/tools/boards/amc/flash/amc_test.bat
@@ -0,0 +1,22 @@
+:start
+@echo off
+
+set PATH=C:\Program Files
+
+"%PATH%\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe" -c port=SWD freq=8000 ap=0 reset=SWrst
+echo;
+"%PATH%\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe" -c port=SWD -d "hex\amc_test.hex" 0x08000000 --verify
+echo;
+"%PATH%\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe" -c port=SWD -Rst -Run
+echo "PRESS ENTER TO CONTINUE"
+echo;
+IF %errorlevel% NEQ 0 GOTO :error
+GOTO :end
+:error
+echo There was an error.
+PAUSE
+EXIT 1
+:end
+echo End.
+PAUSE
+EXIT 0
